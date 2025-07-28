321e70fe5e9004a6302082737bee8cd24f7b4b6a
truongtruong373 <truongtd373@gmail.com>
2025-07-29 00:06:48 +0700

    add mode cluster sequential

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import signal
 from src.Server_sequentail_device import Server
+from src.Server_sequentail_cluster import  Server_cluster
 from src.Utils import delete_old_queues
 import src.Log
 import yaml
@@ -15,6 +16,7 @@ with open('config.yaml') as file:
 address = config["rabbit"]["address"]
 username = config["rabbit"]["username"]
 password = config["rabbit"]["password"]
+cluster = config["server"]["client-cluster"]["enable"]
 virtual_host = config["rabbit"]["virtual-host"]
 
 
@@ -27,6 +29,9 @@ def signal_handler(sig, frame):
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     delete_old_queues(address, username, password, virtual_host)
-    server = Server(config)
+    if cluster:
+        server = Server_cluster(config)
+    else:
+        server = Server(config)
     server.start()
     src.Log.print_with_color("Ok, ready!", "green")
